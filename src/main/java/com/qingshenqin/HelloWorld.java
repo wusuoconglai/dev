44f512b47277e2681478949a541abaec392aa4ac
shanyiyoudao <qingshenqin_ynx@qq.com>
2021-05-31 20:50:19 +0800

    first

---
+++ src/main/java/com/qingshenqin/HelloWorld.java
@@ -0,0 +1,26 @@
+package com.qingshenqin;
+
+/**
+ * @author ： karl
+ * @date ：Created in 2021/5/31 16:55
+ * @description：
+ * @modified By：`
+ * @version: 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world4");
+        System.out.println("hello world4");
+        System.out.println("hello world4");
+        System.out.println("hello world4");
+        System.out.println("hello world4");
+        System.out.println("hello world4");
+        System.out.println("hello world4");
+        System.out.println("hello world4");
+        System.out.println("hello world3");
+    }
+}
